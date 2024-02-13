5f402d058e3d479e141ffb453d6093c209c4b51c
abdelghafour <a.aouad@itroadgroup.com>
2024-02-13 09:36:30 +0100

    update user entity

--- backend/src/main/java/org/example/epicrecipe/entities/User.java
+++ backend/src/main/java/org/example/epicrecipe/entities/User.java
@@ -1,24 +1,28 @@
 package org.example.epicrecipe.entities;
-
 import lombok.Data;
 
 import jakarta.persistence.*;
 import org.example.epicrecipe.entities.enums.Role;
 
 import java.util.List;
-import java.util.UUID;
 
 @Entity
 @Data
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
 
     private String username;
     private String email;
     private String password;
+
+    @ElementCollection(targetClass = Role.class)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private List<Role> roles;
 
     @OneToMany(mappedBy = "user")
     private List<Comment> comments;
@@ -29,11 +33,5 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<SearchHistory> searchHistory;
 
-    @ElementCollection(targetClass = Role.class)
-    @Enumerated(EnumType.STRING)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
-    @Column(name = "role")
-    private List<Role> roles;
-
+    // Constructors, getters, setters, etc.
 }
-
