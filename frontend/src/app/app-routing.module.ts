c6cb15960f34fe6cd36757596d14e3ee0eee0e20
abdelghafour aouad <miso.abdo@gmail.com>
2024-03-13 10:44:54 +0000

    add landing-page conponent

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,44 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { RecipesComponent } from './shared/components/recipes/recipes.component';
-import { AuthGuard } from './core/guards/auth.guard';
-import { UsersComponent } from './shared/components/users/users.component';
-import { RouteListComponent } from './route-list/route-list.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+// import { RecipesComponent } from "./shared/components/recipes/recipes.component";
+// import { AuthGuard } from "./core/guards/auth.guard";
+// import { UsersComponent } from "./shared/components/users/users.component";
+import { RouteListComponent } from "./route-list/route-list.component";
+import { LandingPageComponent } from "./shared/pages/landing-page/landing-page.component";
 
 const routes: Routes = [
-  { path: 'routes', component: RouteListComponent },
-  // {
-  //   path: '',
-  //   redirectTo: 'recipes',
-  //   pathMatch: 'full',
-  // },
-  // {
-  //   path: '**',
-  //   redirectTo: 'recipes',
-  // },
-  {
-    path: 'recipe',
-    loadChildren: () =>
-      import('./modules/recipe/recipe.module').then((m) => m.RecipeModule),
-  },
-  {
-    path: 'auth',
-    loadChildren: () =>
-      import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  },
-  // {
-  //   path: '',
-  //   redirectTo: 'recipes',
-  //   pathMatch: 'full',
-  // },
-  // {
-  //   path: '**',
-  //   redirectTo: 'recipes',
-  // },
+	{ path: "", component: LandingPageComponent },
+	// { path: "routes", component: RouteListComponent },
+	{
+		path: "recipe",
+		loadChildren: () => import("./modules/recipe/recipe.module").then((m) => m.RecipeModule),
+	},
+	{
+		path: "auth",
+		loadChildren: () => import("./modules/auth/auth.module").then((m) => m.AuthModule),
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
