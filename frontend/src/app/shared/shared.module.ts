3f191997d17d5bc83f84680a8d32603f7b00efd1
abdelghafour aouad <miso.abdo@gmail.com>
2024-03-14 15:37:09 +0000

    Add category and speed-dial components

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { SpeedDialComponent } from "./components/speed-dial/speed-dial.component";
+import { CategoryComponent } from "./components/category/category.component";
 
 @NgModule({
-	declarations: [NavbarComponent, FooterComponent],
+	declarations: [NavbarComponent, FooterComponent, SpeedDialComponent, CategoryComponent],
 	imports: [CommonModule],
-	exports: [NavbarComponent, FooterComponent],
+	exports: [NavbarComponent, FooterComponent, SpeedDialComponent, CategoryComponent],
 })
 export class SharedModule {}
