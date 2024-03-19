7d5b7e084acd9ce12ba12f0fa637dc8f9afe065e
abdelghafour aouad <miso.abdo@gmail.com>
2024-03-19 13:04:29 +0000

    get data from api to landing page and recipe details components

--- frontend/src/app/shared/components/card-dishe/card-dishe.component.ts
+++ frontend/src/app/shared/components/card-dishe/card-dishe.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { Recipe } from "app/core/models/recipe";
 
 @Component({
-  selector: 'app-card-dishe',
-  templateUrl: './card-dishe.component.html',
-  styleUrl: './card-dishe.component.css'
+	selector: "app-card-dishe",
+	templateUrl: "./card-dishe.component.html",
+	styleUrl: "./card-dishe.component.css",
 })
 export class CardDisheComponent {
+	@Input() recipes!: Recipe[];
 
+	constructor() {}
 }
