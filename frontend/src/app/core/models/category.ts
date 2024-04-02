0886d138319ec5aff423a3592e76805d2cb29869
abdelghafour aouad <miso.abdo@gmail.com>
2024-04-02 00:25:11 +0000

    Add image property to Category

--- frontend/src/app/core/models/category.ts
+++ frontend/src/app/core/models/category.ts
@@ -1,8 +1,9 @@
 export interface CategoryInterface {
 	id?: number;
 	name?: string;
+	image?: string;
 }
 
 export class Category implements CategoryInterface {
-	constructor(public id?: number, public name?: string) {}
+	constructor(public id?: number, public name?: string, public image?: string) {}
 }
--- frontend/src/app/modules/dashboard/components/categories/categories.component.ts
+++ frontend/src/app/modules/dashboard/components/categories/categories.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Category } from "app/core/models/category";
+import { CategoryService } from "app/core/services/category.service";
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+	selector: "app-categories",
+	templateUrl: "./categories.component.html",
+	styleUrl: "./categories.component.css",
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+	categories: Category[] = [];
+	constructor(private categoryService: CategoryService) {}
 
+	ngOnInit(): void {
+		this.categoryService.getCategories().subscribe((categories) => {
+			this.categories = categories;
+		});
+	}
 }
