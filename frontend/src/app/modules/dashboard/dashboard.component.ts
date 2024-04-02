52f5f9fe4f0bc6b4f0b4ff881ea7def85e988058
abdelghafour aouad <miso.abdo@gmail.com>
2024-04-02 00:25:42 +0000

    Update user profile photo and display user information in dropdown menu

--- frontend/src/app/modules/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Flowbite } from "app/core/config/flowbiteConfig";
 import { initFlowbite } from "flowbite";
+import { KeycloakService } from "keycloak-angular";
+import { KeycloakProfile } from "keycloak-js";
 
 @Component({
 	selector: "app-dashboard",
@@ -9,9 +11,21 @@ import { initFlowbite } from "flowbite";
 })
 @Flowbite()
 export class DashboardComponent implements OnInit {
-	constructor() {}
+	public profile!: KeycloakProfile;
+	userRoles: string[] = [];
+	constructor(public keycloakService: KeycloakService) {}
 
-	ngOnInit() {
+	async ngOnInit() {
 		initFlowbite();
+		const isLoggedIn: boolean = await this.keycloakService.isLoggedIn();
+		if (isLoggedIn) {
+			const profile: KeycloakProfile = await this.keycloakService.loadUserProfile();
+			this.profile = profile;
+			const roles: string[] = await this.keycloakService.getUserRoles();
+			this.userRoles = roles;
+		}
+	}
+	handleLogout() {
+		this.keycloakService.logout(window.location.origin);
 	}
 }
